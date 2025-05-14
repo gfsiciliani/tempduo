2119c5733483fee655ab9ff8deca332a6bfe99b2
gfsiciliani <gfsiciliani@gmail.com>
2025-05-14 15:31:35 -0500

    adding existing script files

---
+++ eq.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# define temp scales
+temp_scales = ['F', 'C']
+
+# take in temp
+temp_in = int(input('temp: '))
+
+# calc functions
+def f_to_c(temp_f):
+    temp_c = (temp_f - 32) * 5/9
+    return temp_c
+def c_to_f(temp_c):
+    temp_f = (temp_c * 9/5) + 32
+    return temp_f
+
+# ask temp type
+while True:
+    temp_scale = input('scale: ').upper()
+
+    if temp_scale == 'F':
+        temp_out = f_to_c(temp_in)
+        temp_out_scale = 'C'
+        break
+    elif temp_scale == 'C':
+        temp_out = c_to_f(temp_in)
+        temp_out_scale = 'F'
+        break
+    else:
+        print(f'invalid input -- please choose from {temp_scales}')
+
+print(f'{temp_out}º {temp_out_scale}')
+
